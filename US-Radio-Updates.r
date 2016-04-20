7aef7ed6b982b99f4ae7793b67816e5b45eb9e76
rpcach <rpcach@users.noreply.github.com>
2016-04-19 23:27:43 -0400

    no message

--- US-Radio-Updates.r
+++ US-Radio-Updates.r
@@ -17,26 +17,26 @@ colnames(mainData)[2] <- as.character.Date(date)
 
 for(i in 2:TOTALDAYS) {
   date <- date-1
-  yesterday <- getDayDataTable(date)
-  yesterday <- yesterday[c("Title","Spins")]
-  colnames(yesterday)[2] <- as.character.Date(date)
+  tempData <- getDayDataTable(date)
+  tempData <- tempData[c("Title","Spins")]
+  colnames(tempData)[2] <- as.character.Date(date)
   
-  mainData <- merge(mainData, yesterday, by="Title")
+  mainData <- merge(mainData, tempData, by="Title")
 }
 
 date <- Sys.Date()
 dateString <- as.character.Date(date)
 
 mainData <- mainData[order(mainData[[dateString]], decreasing = TRUE),]
-xmatrix <- data.matrix(mainData[-1])
-persp(x = 1:nrow(mainData), y = 1:TOTALDAYS, z = xmatrix, phi=40, theta=10)
+mainDataMatrix <- data.matrix(mainData[-1])
+persp(x = 1:nrow(mainData), y = 1:TOTALDAYS, z = mainDataMatrix, phi=40, theta=10)
 
 library("plot3D")
-persp3D(x = 1:nrow(mainData), y = 1:TOTALDAYS, z = xmatrix)
+persp3D(x = 1:nrow(mainData), y = 1:TOTALDAYS, z = mainDataMatrix)
 #size of matrix must be equal to x-by-y in persp function
 
 library("rgl")s
-persp3d(x = 1:nrow(mainData), y = 1:TOTALDAYS, z = xmatrix, col=rainbow(1000), xlab="Song Position", ylab="Time Ago in Days", zlab="Spins")
+persp3d(x = 1:nrow(mainData), y = 1:TOTALDAYS, z = mainDataMatrix, col=rainbow(1000), xlab="Song Position", ylab="Time Ago in Days", zlab="Spins")
 #browseURL(paste("file://", writeWebGL(dir=file.path("radio", "webGL"), width=700), sep=""))
 #creates html file holding interactable 3d-plot
 
