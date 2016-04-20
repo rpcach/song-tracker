80ba8b4168330d9d46090a656303c92921d2a30d
rpcach <rpcach@users.noreply.github.com>
2016-04-19 22:55:42 -0400

    no message

--- US-Radio-Updates.r
+++ US-Radio-Updates.r
@@ -15,8 +15,6 @@ keeps <- c("Title", "Spins")
 mainData <- mainData[keeps]
 colnames(mainData)[2] <- as.character.Date(date)
 
-#barplot(spinData[[1]])
-
 for(i in 2:TOTALDAYS) {
   date <- date-1
   yesterday <- getDayDataTable(date)
@@ -26,18 +24,20 @@ for(i in 2:TOTALDAYS) {
   mainData <- merge(mainData, yesterday, by="Title")
 }
 
-mainData <- mainData[order(mainData[[as.character.Date(Sys.Date())]], decreasing = TRUE),]
+date <- Sys.Date()
+dateString <- as.character.Date(date)
+
+mainData <- mainData[order(mainData[[dateString]], decreasing = TRUE),]
 xmatrix <- data.matrix(mainData[-1])
 persp(x = 1:nrow(mainData), y = 1:TOTALDAYS, z = xmatrix, phi=40, theta=10)
+
 library("plot3D")
 persp3D(x = 1:nrow(mainData), y = 1:TOTALDAYS, z = xmatrix)
 #size of matrix must be equal to x-by-y in persp function
 
-library("rgl")
+library("rgl")s
 persp3d(x = 1:nrow(mainData), y = 1:TOTALDAYS, z = xmatrix, col=rainbow(1000), xlab="Song Position", ylab="Time Ago in Days", zlab="Spins")
 #browseURL(paste("file://", writeWebGL(dir=file.path("radio", "webGL"), width=700), sep=""))
 #creates html file holding interactable 3d-plot
 
-#barplot(current$Spins, names.arg = current$Pos, col=rainbow(50))
-#barplot(yesterday$Spins, names.arg = yesterday$Pos, col=rainbow(50))
-
+barplot(mainData[[dateString]], col=rainbow(50))
