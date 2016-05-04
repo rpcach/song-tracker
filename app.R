8e494fe9249b49ac1f07e35c5e3176ab1844e294
rpcach <rpcach@users.noreply.github.com>
2016-05-04 10:55:48 -0500

    sidebar layout

--- app.R
+++ app.R
@@ -2,20 +2,28 @@ library(shiny)
 source("US-Radio-Updates.r")
 
 ui <- fluidPage(
-  dateRangeInput(inputId = "range",
-                 label = "Date Range",
-                 start = (Sys.Date()-30),
-                 end = Sys.Date(),
-                 min = "2011-05-12",
-                 max = Sys.Date()),
-  uiOutput(outputId = "songs"),
-  plotOutput("songSpinPlot")
+  titlePanel("Song Tracker"),
+  sidebarLayout(
+   sidebarPanel(
+     dateRangeInput(inputId = "range",
+                    label = "Date Range",
+                    start = (Sys.Date()-30),
+                    end = Sys.Date(),
+                    min = "2011-05-12",
+                    max = Sys.Date()),
+     uiOutput(outputId = "songs")
+   ),
+   mainPanel(
+     plotOutput("songSpinPlot")
+   )
+  )
+  
 )
 
 server <- function(input,output) {
   output$songs <- renderUI({
     assign("mainData",loadData(input$range[1],input$range[2]), envir=.GlobalEnv)
-    titles <- levels(mainData$Title)
+    titles <- as.character(mainData$Title)
     checkboxGroupInput(inputId = "songs2",
                        label = "SongsX",
                        choices = titles,
@@ -24,7 +32,7 @@ server <- function(input,output) {
   output$songSpinPlot <- reactivePlot(function() {
     df <- songs2df(input$songs2,input$range[1],input$range[2])
     colnames(df) <- c("Title","Date","Spins")
-    p <- ggplot() + geom_line(data=df, aes(x=Date,y=Spins,col=Title)) + ggtitle("Current Top 5 Songs")
+    p <- ggplot() + geom_line(data=df, aes(x=Date,y=Spins,col=Title))
     print(p)
   })
 }
